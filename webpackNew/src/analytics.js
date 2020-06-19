f88af60230f7bfdc32807b5bd134e1f889a0ded8
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-06-19 12:53:43 +0300

    Initial commit

---
+++ webpackNew/src/analytics.js
@@ -0,0 +1,25 @@
+function createAnalytics() {
+   let counter = 0
+   let destroyed = false
+
+   const listener = () => counter++
+
+   document.addEventListener('click', listener)
+
+   return {
+      destroy() {
+         document.removeEventListener('click', listener)
+         isDestroyed = true
+      },
+
+      getClicks() {
+         if (destroyed) {
+            return `Analytics is destroyed. Total clicks = ${counter}`
+         }
+         return counter
+      }
+   }
+}
+
+window.analytics = createAnalytics()
+
