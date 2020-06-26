99b58666b59a23cf9ae115db1058a212efb4c7ae
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-06-26 15:03:33 +0300

    Added  file-loader

--- webpackNew/src/index.js
+++ webpackNew/src/index.js
@@ -1,6 +1,7 @@
 import Post from './Post'
+import LogoChrome from './assets/logo-chrome.png'
 import './styles/styles.css'
 
-const post = new Post('Webpack Post Title')
+const post = new Post('Webpack Post Title', LogoChrome)
 
 console.log('Post to String:', post.toString())
