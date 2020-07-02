e049471c0ccd37523306bfb7d6c8aae420ec891d
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-07-02 13:17:21 +0300

    added mini-css-extract-plugin & optimize-css-assets-webpack-plugin & terser-webpack-plugin

--- webpackNew/webpack.config.js
+++ webpackNew/webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
 
 module.exports = {
    context: path.resolve(__dirname, 'src'),
@@ -27,24 +32,41 @@ module.exports = {
       }
    },
    devServer: {
-      port: 4200
+      port: 4200,
+      hot: isDev
    },
    plugins: [
       new HTMLWebpackPlugin({
-         template: './index.html'
+         template: './index.html',
+         minify: {
+            collapseWhitespace: isProd
+         }
       }),
       new CleanWebpackPlugin(),
       new CopyWebpackPlugin({
          patterns: [
-             { from: 'favicon.ico', to: '../dist' }
+             {
+                 from: path.resolve(__dirname, "src/favicon.ico"),
+                 to: path.resolve(__dirname, "dist")
+             },
          ]
-      })
+     }),
+     new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+     })
    ],
    module: {
       rules: [
          {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [
+               {
+                  loader: MiniCssExtractPlugin.loader,
+                  options: {
+                     hmr: isDev,
+                     reloadAll: true,
+                  },
+               }, 'css-loader']
          },
          {
             test: /\.(png|jpg|svg|gif)$/,
