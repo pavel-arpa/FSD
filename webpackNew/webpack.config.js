0512af481ce207c3c01da07c97c719142dd7201c
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-07-14 21:16:23 +0300

    Added PUG

--- webpackNew/webpack.config.js
+++ webpackNew/webpack.config.js
@@ -1,12 +1,23 @@
+
+// БЛОК ОПИСАНИЯ КОНСТАНТ
 const path = require('path')
+const fs = require('fs')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimazeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 
+// Путь через dirname
+const PATHS = {
+   src: path.join(__dirname, './src'),
+   dist: path.join(__dirname, './dist'),
+   assets: 'assets/'
+ }
 
+// Определение мода: разработка или продакшн
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
@@ -27,6 +38,11 @@ optimization = () => {
 
 const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
 
+// Константы для PUG 
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+// Функция автодобавления кода для cssLoaders 
 const cssLoaders = extra => {
    const loaders = [
       {
@@ -44,6 +60,10 @@ const cssLoaders = extra => {
    }
    return loaders
 }
+
+// ---------------------------------- ++++ ----------------------------------
+
+
 
 module.exports = {
    context: path.resolve(__dirname, 'src'),
@@ -69,12 +89,28 @@ module.exports = {
       hot: isDev
    },
    plugins: [
+      // ...PAGES.map(page => new HtmlWebpackPlugin(
+      //    {
+      //       template: `${PAGES_DIR}/${page}`,
+      //       filename: `./${page.replace(/\.pug/,'.html')}`,
+      //       minify: {
+      //          collapseWhitespace: isProd
+      //       }
+      //    }
+      //  )),
+      // new HtmlWebpackPlugin({
+      //    // template: `${PAGES_DIR}/index.pug`,
+      //    template: './src/pug/pages/index.pug',
+      //    filename: './index.html',
+      //    inject: true
+      // }),
       new HTMLWebpackPlugin({
-         template: './index.html',
+         template: './pug/pages/index.pug',
          minify: {
             collapseWhitespace: isProd
          }
       }),
+      new HtmlWebpackPugPlugin(),
       new CleanWebpackPlugin(),
       new CopyWebpackPlugin({
          patterns: [
@@ -90,6 +126,10 @@ module.exports = {
    ],
    module: {
       rules: [
+         {
+            test: /\.pug$/,
+            loader: 'pug-loader'
+         },
          {
             test: /\.css$/,
             use: cssLoaders()
