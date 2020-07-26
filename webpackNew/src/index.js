0ea9979f16bde169aba85280935c9706bc14ca20
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-07-26 23:20:27 +0300

    Подключил шрифты, а также продолжал верстать первую страницу UI Kit

--- webpackNew/src/index.js
+++ webpackNew/src/index.js
@@ -1,8 +1,8 @@
 import * as $ from 'jquery'
 import LogoChrome from './assets/logo-chrome.png'
 
-function importJsCss(resolve) {
-   resolve.keys().forEach(resolve);
-}
+// function importJsCss(resolve) {
+//    resolve.keys().forEach(resolve);
+// }
 
-importJsCss(require.context('../src/', true, /\.js$|\.scss$/));
+// importJsCss(require.context('../src/', true, /\.js$|\.scss$/));
--- webpackNew/src/uikit.js
+++ webpackNew/src/uikit.js
@@ -1,5 +1,8 @@
-function importJsCss(resolve) {
-   resolve.keys().forEach(resolve);
-}
+// function importJsCss(resolve) {
+//    resolve.keys().forEach(resolve);
+// }
+// importJsCss(require.context('../src/', true, /\.js$|\.scss$/));
 
-importJsCss(require.context('../src/', true, /\.js$|\.scss$/));
+import './styles/null.scss'
+import './styles/uikit.scss'
+
