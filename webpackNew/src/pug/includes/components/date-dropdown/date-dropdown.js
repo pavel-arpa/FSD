ff89cb968f2ea6ccb697823de2223afcb427cf1c
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-10-01 09:01:44 +0300

    Продолжаю работу над date-dropdown

--- webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
+++ webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
@@ -2,42 +2,86 @@
 const len = document.querySelectorAll('.date-dropdown').length
 let allDropdowns = document.querySelectorAll('.date-dropdown')
 
-// Массив для datepickers (от плагина)
-let pluginsDatePickers = []
+
+// Локализация
+$.fn.datepicker.language['ru'] =  {
+   days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+   daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
+   daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+   months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+   monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек'],
+   today: 'Применить',
+   clear: 'Очистить',
+   dateFormat: 'dd.mm.yyyy',
+   timeFormat: 'hh:ii',
+   firstDay: 1
+}
+
 
 // Присваеваем каждому date-dropdown свой id для уникальности и
 // удобств их обработки
 for (let x=0; x<len; x++) {
    let newId = "dateDropdown-" + x
-   let inputs = allDropdowns[x].querySelectorAll('.date-dropdown__input')
+   let inputs = []
+   inputs = allDropdowns[x].querySelectorAll('.date-dropdown__input')
 
-   inputs[0].id = newId
-   inputs[0].value = ''
-   inputs[1].value = ''
-
-
-   // Опции календаря
-   $('#'+newId).datepicker({
-      todayButton: new Date(),
-      clearButton: true,
-      range: true,
-      minDate: new Date(),
-      navTitles: {
-         days: 'MM yyyy'
-      },
-      onSelect: function(formattedDate, date, inst) {
-         inputs[0].value = formattedDate.split(' - ')[0]
-         if (!(formattedDate.split(' - ')[1] == undefined)) {
-            inputs[1].value = formattedDate.split(' - ')[1]
+   if (inputs.length == 2) {
+      inputs[0].id = newId
+      inputs[0].value = ''
+      inputs[1].value = ''
+      
+      // Сохранение результата даты
+      let savingFormatDate
+      
+      // Опции календаря
+      $('#'+newId).datepicker({
+         todayButton: new Date(),
+         clearButton: true,
+         range: true,
+         minDate: new Date(),
+         navTitles: {
+            days: 'MM yyyy'
+         },
+         onSelect: function(formattedDate, date, inst) {
+            inputs[0].value = formattedDate.split(' - ')[0]
+            savingFormatDate = formattedDate.split(' - ')[0]
+            
+            if (!(formattedDate.split(' - ')[1] == undefined)) {
+               inputs[1].value = formattedDate.split(' - ')[1]
+            }
+            
+            if (formattedDate == '') {
+               inputs[0].value = ''
+               inputs[1].value = ''
+            }
          }
-      }
-   })
+      })
 
-   pluginsDatePickers.push(document.querySelector('.datepicker'))
-   console.log(pluginsDatePickers)
-   inputs[1].addEventListener('click', function() {
-      if (!pluginsDatePickers[x].classList.contains('active')) {
-         $('#'+newId).datepicker().data('datepicker').show()
-      }
-   })
+      const acceptButton = document.querySelectorAll("[data-action='today']")
+      $(acceptButton).on('click', function() {
+         $(inputs[0]).data('datepicker').hide()
+      })
+      
+      inputs[1].addEventListener('click', function() {
+         if (!document.querySelector('.datepicker').classList.contains('active')) {
+            $('#'+newId).datepicker().data('datepicker').show()
+            // Решение проблемы автозаполнения плагином первого инпута
+            // другим форматом путем переприсваивания значения инпута
+            savingFormatDate == undefined ? true : inputs[0].value = savingFormatDate
+         }
+      })
+   } else {
+      inputs[0].id = newId
+      
+      $('#'+newId).datepicker({
+         todayButton: new Date(),
+         clearButton: true,
+         range: true,
+         minDate: new Date(),
+         navTitles: {
+            days: 'MM yyyy'
+         },
+         dateFormat: 'd M'
+      })
+   }
 }
