b90b09596f0c41191735af6d8edad39f1b30e8cb
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-10-03 22:32:14 +0300

    Стилизация календаря. Сделал всё, кроме линии между датами (не полностью отображается)

--- webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
+++ webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
@@ -42,6 +42,8 @@ for (let x=0; x<len; x++) {
          navTitles: {
             days: 'MM yyyy'
          },
+         nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
+         prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
          onSelect: function(formattedDate, date, inst) {
             inputs[0].value = formattedDate.split(' - ')[0]
             savingFormatDate = formattedDate.split(' - ')[0]
@@ -77,7 +79,9 @@ for (let x=0; x<len; x++) {
          navTitles: {
             days: 'MM yyyy'
          },
-         dateFormat: 'd M'
+         dateFormat: 'd M',
+         nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
+         prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
       })
    }
    const acceptButton = document.querySelectorAll("[data-action='today']")
@@ -87,3 +91,15 @@ for (let x=0; x<len; x++) {
       })
    }
 }
+$('.datepicker-here').datepicker({
+   todayButton: new Date(),
+   clearButton: true,
+   range: true,
+   minDate: new Date(),
+   navTitles: {
+      days: 'MM yyyy'
+   },
+   // dateFormat: 'd M',
+   nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
+   prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
+})
