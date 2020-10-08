d925b47f80224d14753efee567e44f293af97ee6
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-10-08 08:58:18 +0300

    Стилизация dateDropdown закончена

--- webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
+++ webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
@@ -103,3 +103,14 @@ $('.datepicker-here').datepicker({
    nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
    prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
 })
+
+// Обработка события с полосой между выбранными днями для -range-from-
+// let cells = document.querySelector('.datepicker--cells')
+
+//    setInterval(function() {
+//       let start = cells.querySelector('.-range-from-')
+//       if (start.nextSibling.classList.contains('.-in-range-') || start.nextSibling.classList.contains('.-range-to-')) {
+//          start.classList.toggle('.-for-range-from-style-')
+//          console.log(1)
+//       }
+//    }, 20)
