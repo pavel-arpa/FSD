ae9b3fcf38ce66617ae0917b110e902da7b3dd79
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-10-09 17:33:28 +0300

    Доделал вторую форму с подсчетом стоимости

--- webpackNew/src/pug/includes/components/calc-form/calc-form.js
+++ webpackNew/src/pug/includes/components/calc-form/calc-form.js
@@ -1,32 +1,71 @@
 let calcForm = document.querySelector('.calc-form')
-let price = calcForm.querySelector('.calc-form__price')
-let inputs = calcForm.querySelectorAll('.date-dropdown__input')
+let priceOfRoom = calcForm.querySelector('.calc-form__price')
+let inputsDate = calcForm.querySelectorAll('.date-dropdown__input')
+let calcStrings = calcForm.querySelectorAll('.calc-form__calc-string')
+let totals = calcForm.querySelectorAll('.calc-form__total')
+let result = calcForm.querySelector('.calc-form__result')
 
-price.innerHTML = 9990
+let values = {
+  price: 9990,
+  service: 0,
+  additional: 300,
+  discount: 2179,
+}
+
+let dates = {
+  first: 0,
+  second: 0,
+  toCountDays() {
+    return ((dates.second - this.first) / (1000 * 3600 * 24))
+  },
+  toConjugate(days) {
+    if (days % 10 == 1) {
+      return `${days} сутки`
+    } else {
+      return `${days} суток`
+    }
+  }
+}
+
+priceOfRoom = values.price
+
+inputsDate[0].addEventListener('blur', function() {
+  dates.first = new Date(inputsDate[0].value.split('.').reverse().join('-'))
+  dates.second = new Date(inputsDate[1].value.split('.').reverse().join('-'))
+
+  if(!isNaN(dates.toCountDays())) {
+    // Цена за количество дней
+    calcStrings[0].innerHTML = `${values.price}₽ x ${dates.toConjugate(dates.toCountDays())}`
+    totals[0].innerHTML = values.price * dates.toCountDays()
+
+    // Услуги
+    if (values.discount != 0) {
+      calcStrings[1].innerHTML = `Сбор за услуги: скидка ${values.discount}₽`
+      totals[1].innerHTML = values.service
+    } else {
+      calcStrings[1].innerHTML = 'Сбор за услуги: '
+      totals[1].innerHTML = values.service
+    }
+
+    // Доп услуги
+    calcStrings[2].innerHTML = 'Сбор за дополнительные услуги:'
+    totals[2].innerHTML = values.additional
+
+    // Итого
+    result.innerHTML = (values.price * dates.toCountDays()) - values.discount + values.additional
+
+  } else {
+    calcStrings[0].innerHTML = 'Цена за проживание:'
+    totals[0].innerHTML = 0
+    calcStrings[1].innerHTML = 'Сбор за услуги:'
+    totals[1].innerHTML = 0
+    calcStrings[2].innerHTML = 'Сбор за дополнительные услуги:'
+    totals[2].innerHTML = 0
+    result.innerHTML = 0
+  }
+})
 
 
-setInterval(function() {
-  console.log(inputs[0].value)
-  // let firstDate = new Date(inputs[0].value.split('.').join('-'))
-  // let secondDate = new Date(inputs[1].value.split('.').join('-'))
-  // let dateRange = secondDate - firstDate
-  // console.log(dateRange)
-}, 200)
-// $(inputs[1]).on('blur', function() {
-//   alert(1)
-// })
-// $(inputs[1]).val("").change(function() {
-//   alert(1)
-// })
-
-// inputs[1].value.addEventListener('change', function(e) {
-//   console.log(1)
-//   let firstDate = new Date(inputs[0].value.split('.').join('-'))
-//   let secondDate = new Date(inputs[1].value.split('.').join('-'))
-//   let dateRange = secondDate - firstDate
-//   console.log(dateRange)
-// })
-// console.log(inputs[1])
 
 
 
--- webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
+++ webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
@@ -17,16 +17,6 @@ $.fn.datepicker.language['ru'] =  {
    firstDay: 1
 }
 
-let inputsArr = {
-   newValue(arg) {
-      x = []
-      x.push(arg)
-      x = toString(x)
-      if (x)
-      this.x = []
-
-   }
-}
 
 // Присваеваем каждому date-dropdown свой id для уникальности и
 // удобств их обработки
@@ -34,12 +24,13 @@ for (let x=0; x<len; x++) {
    let newId = "dateDropdown-" + x
    let inputs = []
    inputs = allDropdowns[x].querySelectorAll('.date-dropdown__input')
-
+   
    if (inputs.length == 2) {
       inputs[0].id = newId
       inputs[0].value = ''
       inputs[1].value = ''
       
+   
       // Сохранение результата даты
       let savingFormatDate
       
@@ -57,7 +48,7 @@ for (let x=0; x<len; x++) {
          onSelect: function(formattedDate, date, inst) {
             inputs[0].value = formattedDate.split(' - ')[0]
             savingFormatDate = formattedDate.split(' - ')[0]
-            inputsArr.newValue()
+
             if (!(formattedDate.split(' - ')[1] == undefined)) {
                inputs[1].value = formattedDate.split(' - ')[1]
             }
@@ -113,14 +104,3 @@ $('.datepicker-here').datepicker({
    nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
    prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
 })
-
-// Обработка события с полосой между выбранными днями для -range-from-
-// let cells = document.querySelector('.datepicker--cells')
-
-//    setInterval(function() {
-//       let start = cells.querySelector('.-range-from-')
-//       if (start.nextSibling.classList.contains('.-in-range-') || start.nextSibling.classList.contains('.-range-to-')) {
-//          start.classList.toggle('.-for-range-from-style-')
-//          console.log(1)
-//       }
-//    }, 20)
