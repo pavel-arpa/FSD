f30b8cea5eadb02b773e516657bd7a5b5778cb6e
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-11-05 21:24:56 +0300

    Update landing-page.js

--- webpackNew/src/landing-page.js
+++ webpackNew/src/landing-page.js
@@ -1,2 +1,7 @@
 import './jquery.maskedinput.min.js'
 import './styles/landing-page.scss'
+
+import '../node_modules/air-datepicker/dist/js/datepicker.js'
+import '../node_modules/air-datepicker/dist/css/datepicker.css'
+import './pug/includes/components/date-dropdown/date-dropdown.js'
+import './pug/includes/components/dropdown/dropdown.js'
