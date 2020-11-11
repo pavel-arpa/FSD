cc8f60e1a331fdf94a67f40ce742a67c899315e4
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-11-11 16:41:10 +0300

    Создано меню для адаптивности в header

---
+++ webpackNew/src/pug/includes/components/header/header.js
@@ -0,0 +1,21 @@
+let iconMenu = document.querySelector('.header__icon-menu')
+let menu = document.querySelector('.header__menu')
+let menuBg = document.querySelector('.header__menu-bg')
+let close = document.querySelector('.header__close')
+let page = document.querySelector('.landing-page')
+
+iconMenu.addEventListener('click', function() {
+  menu.style.left = '0px'
+  menu.style.backgroundColor = 'rgba(0, 0, 0, 0.3)'
+  menuBg.style.margin = '0 0 0 auto'
+  page.style.overflow = 'hidden'
+})
+
+close.addEventListener('click', function() {
+  menu.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+  setTimeout(function() {
+    menu.style.left = '100%'
+  }, 700)
+  menuBg.style.margin = '0 -400px 0 auto'
+  page.style.overflow = 'auto'
+})
