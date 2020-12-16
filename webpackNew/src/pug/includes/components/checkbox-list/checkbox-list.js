3c1aabd058a06d80b1c04b66da541338b8718c54
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-12-16 21:31:54 +0300

    Теперь expandable checkbox list работает всегда

--- webpackNew/src/pug/includes/components/checkbox-list/checkbox-list.js
+++ webpackNew/src/pug/includes/components/checkbox-list/checkbox-list.js
@@ -1,8 +1,13 @@
+if (document.querySelector('.checkbox-list__btn_exp')) {
+   let allCheckboxes = document.querySelectorAll('.checkbox-list__btn_exp')
+   for (let i = 0; i < allCheckboxes.length; i++) {
 
-   $('#expandableCheckbox').click(function(){
-      if ($('#expandableCheckbox').hasClass('checkbox-list__btn_clicked')) {
-         $(this).addClass('checkbox-list__btn_default').removeClass('checkbox-list__btn_clicked');
-      } else {
-         $(this).addClass('checkbox-list__btn_clicked').removeClass('checkbox-list__btn_default');
-      }
-   })
+      $(allCheckboxes[i]).click(function(){
+         if ($(allCheckboxes[i]).hasClass('checkbox-list__btn_clicked')) {
+            $(this).addClass('checkbox-list__btn_default').removeClass('checkbox-list__btn_clicked');
+         } else {
+            $(this).addClass('checkbox-list__btn_clicked').removeClass('checkbox-list__btn_default');
+         }
+      })
+   }
+}
