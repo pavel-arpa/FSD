7e9689a714a066a9bf50c2b2fb0d9c1b4afbabcf
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-12-16 21:32:16 +0300

    Update room-page.js

--- webpackNew/src/pug/pages/room-page.js
+++ webpackNew/src/pug/pages/room-page.js
@@ -1,32 +1,28 @@
-let roomPageImagesLen = document.querySelector('.room-page__common-sector').querySelectorAll('.room-page__image').length
-
-for (let i = 0; i < roomPageImagesLen; i++) {
-  (function() {
-    let leftBut = document.querySelector('.room-page__wrapper-bg-arrow.room-page__wrapper-bg-arrow_left')
-    let rightBut = document.querySelector('.room-page__wrapper-bg-arrow.room-page__wrapper-bg-arrow_right')
-    let current = 0
-    let image = document.querySelector('.room-page__common-sector').querySelectorAll('.room-page__image')
-
-    leftBut.addEventListener('click', function() {
-      image[current].className = 'room-page__image room-page__image_sliding'
-      // onePoint[current].className = 'room-page__one-point'
-      if (current == 0) {
-        current = image.length
-      }
-      current--
-      image[current].className = 'room-page__image room-page__image_current'
-      // onePoint[current].className = 'room-page__one-point room-page__one-point_current'
-    })
-    
-    rightBut.addEventListener('click', function() {
-      image[current].className = 'room-page__image room-page__image_sliding'
-      // onePoint[current].className = 'room-page__one-point'
-      if (current == (image.length - 1)) {
-        current = -1
-      }
-      current++
-      image[current].className = 'room-page__image room-page__image_current'
-      // onePoint[current].className = 'room-page__one-point room-page__one-point_current'
-    })
-  } ())
+if (document.querySelector('.room-page')) {
+  let roomPageImagesLen = document.querySelector('.room-page__common-sector').querySelectorAll('.room-page__image').length
+  
+  for (let i = 0; i < roomPageImagesLen; i++) {
+      let leftBut = document.querySelector('.room-page__wrapper-bg-arrow.room-page__wrapper-bg-arrow_left')
+      let rightBut = document.querySelector('.room-page__wrapper-bg-arrow.room-page__wrapper-bg-arrow_right')
+      let current = 0
+      let image = document.querySelector('.room-page__common-sector').querySelectorAll('.room-page__image')
+  
+      leftBut.addEventListener('click', function() {
+        image[current].className = 'room-page__image room-page__image_sliding'
+        if (current == 0) {
+          current = image.length
+        }
+        current--
+        image[current].className = 'room-page__image room-page__image_current'
+      })
+      
+      rightBut.addEventListener('click', function() {
+        image[current].className = 'room-page__image room-page__image_sliding'
+        if (current == (image.length - 1)) {
+          current = -1
+        }
+        current++
+        image[current].className = 'room-page__image room-page__image_current'
+      })
+  }
 }
