15ee4592b45377c67bc25ce3f4dace9e621e3145
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-12-16 22:10:25 +0300

    build in docs

--- webpackNew/webpack.config.js
+++ webpackNew/webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
    },
    output: {
       filename: filename('js'),
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'docs')
    },
    resolve: {
       extensions: ['.js', 'json', '.png'],
@@ -121,19 +121,19 @@ module.exports = {
          patterns: [
             {
                from: path.resolve(__dirname, 'src/favicon.ico'),
-               to: path.resolve(__dirname, 'dist/assets-img')
+               to: path.resolve(__dirname, 'docs/assets-img')
             },
             {
                from: path.resolve(__dirname, './src/pug/includes/components/feedback/accounts'),
-               to: path.resolve(__dirname, 'dist/img')
+               to: path.resolve(__dirname, 'docs/img')
             },
             {
               from: path.resolve(__dirname, './src/pug/includes/components/room-card/images'),
-              to: path.resolve(__dirname, 'dist/rooms-images')
+              to: path.resolve(__dirname, 'docs/rooms-images')
             },
             {
                from: path.resolve(__dirname, './src/assets/img-for-page-bg'),
-               to: path.resolve(__dirname, 'dist/img-for-page-bg')
+               to: path.resolve(__dirname, 'docs/img-for-page-bg')
             },
          ]
       }),
