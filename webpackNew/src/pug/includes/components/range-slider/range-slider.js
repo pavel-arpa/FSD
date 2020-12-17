ad83a7906cef60d9c0109e11d935e913057982d7
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-12-17 16:40:08 +0300

    Попытка исправить range-slider

--- webpackNew/src/pug/includes/components/range-slider/range-slider.js
+++ webpackNew/src/pug/includes/components/range-slider/range-slider.js
@@ -50,11 +50,42 @@ function rangeSlider(id) {
 
       function onMouseUp() {
          document.removeEventListener('mouseup', onMouseUp);
-         document.removeEventListener('touchup', onMouseUp);
          document.removeEventListener('mousemove', onMouseMove);
+      }
+   }
+   point1.ontouchstart = function(event) {
+      let shiftX = event.clientX - point1.getBoundingClientRect().left + pageXOffset
+      document.addEventListener('touchmove', onMouseMove);
+      document.addEventListener('touchend', onMouseUp);
+
+      function onMouseMove(event) {
+         newLeftFirst = event.pageX - shiftX - slider.getBoundingClientRect().left
+         newLeftFirst = Math.ceil(newLeftFirst / 16.8) * 16.8
+
+         if(newLeftFirst < 0) {
+            newLeftFirst = 0
+         } else if(newLeftFirst >= newLeftSecond) {
+            newLeftFirst = newLeftSecond - 16.8
+         }
+         
+         // Изменение значения диапазона цены
+         leftValue = Math.ceil((newLeftFirst * 59.5) / 1000) * 1000
+         priceRange.innerHTML = `${leftValue}₽ - ${rightValue}₽`
+
+         point1.style.marginLeft = newLeftFirst - 7 - 1 + 'px'
+
+         stripeGradient.style.marginLeft = newLeftFirst + 1 + 'px'
+         stripeGradient.style.paddingRight = newLeftSecond - newLeftFirst + 'px'
+      }
+
+      function onMouseUp() {
+         document.removeEventListener('touchend', onMouseUp);
          document.removeEventListener('touchmove', onMouseMove);
       }
    }
+
+
+
 
    point2.onmousedown = function(event) {
       event.preventDefault() // предотвратить запуск выделения (действие браузера)
@@ -85,8 +116,38 @@ function rangeSlider(id) {
 
       function onMouseUp() {
          document.removeEventListener('mouseup', onMouseUp);
-         document.removeEventListener('touchup', onMouseUp);
          document.removeEventListener('mousemove', onMouseMove);
+      }
+   }
+   point2.ontouchstart = function(event) {
+      event.preventDefault() // предотвратить запуск выделения (действие браузера)
+
+      let shiftX = event.clientX - point2.getBoundingClientRect().left + pageXOffset
+
+      document.addEventListener('touchmove', onMouseMove);
+      document.addEventListener('touchend', onMouseUp);
+
+      function onMouseMove(event) {
+         newLeftSecond = event.pageX - shiftX - slider.getBoundingClientRect().left
+         newLeftSecond = Math.ceil(newLeftSecond / 16.8) * 16.8
+
+         if(newLeftSecond > 252) {
+            newLeftSecond = 252
+         } else if(newLeftSecond <= newLeftFirst) {
+            newLeftSecond = newLeftFirst + 16.8
+         }
+
+         // Изменение значения диапазона цены
+         rightValue = Math.ceil((newLeftSecond * 59.5) / 1000) * 1000
+         priceRange.innerHTML = `${leftValue}₽ - ${rightValue}₽`
+         
+         point2.style.marginLeft = newLeftSecond - 7 - 1 + 'px'
+
+         stripeGradient.style.paddingRight = newLeftSecond - newLeftFirst + 'px'
+      }
+
+      function onMouseUp() {
+         document.removeEventListener('touchstart', onMouseUp);
          document.removeEventListener('touchmove', onMouseMove);
       }
    }
