7fff0feac77243437aac8511983d2ec26192f27c
pavel-arpa <arepev.p.work@users.noreply.github.com>
2020-12-17 16:39:50 +0300

    Update date-dropdown.js

--- webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
+++ webpackNew/src/pug/includes/components/date-dropdown/date-dropdown.js
@@ -89,9 +89,10 @@ for (let x=0; x<len; x++) {
       })
    } else {
       inputs[0].id = newId
-      
+      let savingFormatDate
+
       $('#'+newId).datepicker({
-         todayButton: new Date(),
+         todayButton: true,
          clearButton: true,
          range: true,
          minDate: new Date(),
@@ -101,14 +102,25 @@ for (let x=0; x<len; x++) {
          dateFormat: 'd M',
          nextHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/></svg>',
          prevHtml: '<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/></svg>',
+         onSelect: function(formattedDate, date, inst) {
+            savingFormatDate = formattedDate.split(' - ')[0]
+         },
       })
    }
-   const acceptButton = document.querySelectorAll("[data-action='today']")
-   for (let i = 0; i < acceptButton.length; i++) {
-      acceptButton[i].addEventListener('click', function() {
-         $('#'+newId).data('datepicker').hide()
-      })
-   }
+   let content = document.querySelector('.datepicker--content')
+   console.log(content)
+   document.querySelector('.datepicker--buttons').remove()
+   content.insertAdjacentHTML('afterend', '<div class="datepicker--buttons"><span class="datepicker--button">Применить</span><span class="datepicker--button">Очистить</span></div>')
+   let buttons = document.querySelector('.datepicker--buttons')
+   console.log(buttons)
+   buttons.children[0].addEventListener('click', () => {
+      $('#'+newId).datepicker().data('datepicker').hide()
+      inputs[0].value = savingFormatDate
+   })
+   buttons.children[1].addEventListener('click', () => {
+      $('#'+newId).datepicker().data('datepicker').clear()
+   })
+
 }
 $('.datepicker-here').datepicker({
    todayButton: new Date(),
